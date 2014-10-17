377553f9bac982eb09ac71da12840cff2339059e
Moln <xiemaomao520@163.com>
2014-10-17 16:34:17 +0800

    Fix src

--- src/Hbase/THBaseService.php
+++ src/Hbase/THBaseService.php
@@ -1,10 +1,7 @@
 <?php
 namespace Hbase;
 
-use Thrift\Exception\TApplicationException;
-use Thrift\Exception\TProtocolException;
 use Thrift\Protocol\TProtocol;
-use Thrift\Type\TMessageType;
 use Thrift\Type\TType;
 
 interface THBaseServiceIf
@@ -13,17 +10,17 @@ interface THBaseServiceIf
 
     public function get($table, TGet $get);
 
-    public function getMultiple($table, $gets);
+    public function getMultiple($table, array $gets);
 
     public function put($table, TPut $put);
 
     public function checkAndPut($table, $row, $family, $qualifier, $value, TPut $put);
 
-    public function putMultiple($table, $puts);
+    public function putMultiple($table, array $puts);
 
     public function deleteSingle($table, TDelete $deleteSingle);
 
-    public function deleteMultiple($table, $deletes);
+    public function deleteMultiple($table, array $deletes);
 
     public function checkAndDelete($table, $row, $family, $qualifier, $value, TDelete $deleteSingle);
 
@@ -145,9 +142,9 @@ class THBaseServiceClient implements THBaseServiceIf
     /**
      * @param string $table
      * @param \Hbase\TGet[] $gets
-     * @return \Hbase\TGet[]
+     * @return \Hbase\TResult[]
      */
-    public function getMultiple($table, $gets)
+    public function getMultiple($table, array $gets)
     {
         $args   = array(
             1 => array(
@@ -284,7 +281,7 @@ class THBaseServiceClient implements THBaseServiceIf
      * @param \Hbase\TPut[] $puts
      * @return mixed
      */
-    public function putMultiple($table, $puts)
+    public function putMultiple($table, array $puts)
     {
         $args   = array(
             1 => array(
@@ -352,7 +349,7 @@ class THBaseServiceClient implements THBaseServiceIf
      * @param \Hbase\TDelete[] $deletes
      * @return \Hbase\TDelete[]
      */
-    public function deleteMultiple($table, $deletes)
+    public function deleteMultiple($table, array $deletes)
     {
         $args   = array(
             1 => array(
@@ -565,7 +562,7 @@ class THBaseServiceClient implements THBaseServiceIf
     /**
      * @param  $scannerId
      * @param  $numRows
-     * @return mixed
+     * @return \Hbase\TResult[]
      */
     public function getScannerRows($scannerId, $numRows)
     {
@@ -585,7 +582,7 @@ class THBaseServiceClient implements THBaseServiceIf
             0 => array(
                 'var'   => 'success',
                 'type'  => TType::LST,
-                'etype' => 12,
+                'etype' => TType::STRUCT,
                 'elem'  => array(
                     'type'  => TType::STRUCT,
                     'class' => '\\Hbase\\TResult',
@@ -673,7 +670,7 @@ class THBaseServiceClient implements THBaseServiceIf
      * @param string $table
      * @param \Hbase\TScan $scan
      * @param  $numRows
-     * @return mixed
+     * @return \Hbase\TResult[]
      */
     public function getScannerResults($table, TScan $scan, $numRows)
     {
@@ -699,7 +696,7 @@ class THBaseServiceClient implements THBaseServiceIf
             0 => array(
                 'var'   => 'success',
                 'type'  => TType::LST,
-                'etype' => 12,
+                'etype' => TType::STRUCT,
                 'elem'  => array(
                     'type'  => TType::STRUCT,
                     'class' => '\\Hbase\\TResult',
@@ -714,243 +711,5 @@ class THBaseServiceClient implements THBaseServiceIf
 
         $this->sendCommand(__FUNCTION__, $args);
         return $this->recv($result);
-    }
-}
-
-
-/**
- * Class TProtocolController
- *
- * @package Hbase
- */
-class TProtocolController
-{
-
-    protected $input = null;
-    protected $output = null;
-
-    protected $seqid = 0;
-
-    public function __construct(TProtocol $input, TProtocol $output = null)
-    {
-        $this->input  = $input;
-        $this->output = $output ? : $input;
-    }
-
-    private function write($args)
-    {
-        $xfer = 0;
-        $xfer += $this->output->writeStructBegin(get_class($this));
-
-        foreach ($args as $key => $arg) {
-            if ($arg['value'] !== null) {
-                $this->writeArgByType($key, $arg, $xfer);
-            }
-        }
-
-        $xfer += $this->output->writeFieldStop();
-        $xfer += $this->output->writeStructEnd();
-        return $xfer;
-    }
-
-    private function writeArgByType($key, $item, &$xfer)
-    {
-        $output = $this->output;
-        $type   = $item['type'];
-
-        $callbacks = array(
-            TType::BOOL   =>
-                function (&$xfer, $item) use ($output) {
-                    $xfer += $output->writeBool($item['value']);
-                },
-            TType::STRING =>
-                function (&$xfer, $item) use ($output) {
-                    $xfer += $output->writeString($item['value']);
-                },
-            TType::STRUCT =>
-                function (&$xfer, $item) use ($output) {
-                    if (is_object($item['value']) && isset($item['value']::$_TSPEC)) {
-
-                        $subArgs = $item['value']::$_TSPEC;
-                        foreach ($subArgs as &$subArg) {
-                            $subArg['value'] = $item['value']->{$subArg['var']};
-                        }
-
-                        $xfer += $this->write($subArgs);
-                    } else {
-                        throw new TProtocolException('Bad type in structure.', TProtocolException::INVALID_DATA);
-                    }
-                },
-        );
-
-        $callbacks[TType::LST] = function (&$xfer, $items) use ($output, $callbacks) {
-            if (!is_array($items['value'])) {
-                throw new TProtocolException('Bad type in structure.', TProtocolException::INVALID_DATA);
-            }
-            $output->writeListBegin($items['etype'], count($items['value']));
-
-            foreach ($items['value'] as $item) {
-                $callbacks[$items['elem']['type']]($xfer, $item);
-            }
-            $output->writeListEnd();
-            $xfer += $output->writeFieldEnd();
-        };
-
-        if (isset($callbacks[$type])) {
-            $xfer += $output->writeFieldBegin($item['var'], $type, $key);
-            $callbacks[$type]($xfer, $item);
-            $xfer += $output->writeFieldEnd();
-        } else {
-            throw new \InvalidArgumentException('Invalid type:' . $type);
-        }
-    }
-
-    /**
-     * @param $args
-     * @return int
-     */
-    private function read(&$args)
-    {
-        $xfer  = 0;
-        $fname = null;
-        $ftype = 0;
-        $fid   = 0;
-        $xfer += $this->input->readStructBegin($fname);
-        while (true) {
-            $xfer += $this->input->readFieldBegin($fname, $ftype, $fid);
-            if ($ftype == TType::STOP) {
-                break;
-            }
-
-            if (isset($args[$fid])) {
-                $this->setArg($args[$fid], $ftype, $xfer);
-            } else {
-                $xfer += $this->input->skip($ftype);
-                break;
-            }
-
-            $xfer += $this->input->readFieldEnd();
-        }
-        $xfer += $this->input->readStructEnd();
-        return $xfer;
-    }
-
-
-    private function setArg(&$item, $ftype, &$xfer)
-    {
-        $type          = $item['type'];
-        $item['value'] = null;
-
-        $callbacks = array(
-            TType::BOOL   =>
-                function (&$item, &$xfer) {
-                    $xfer += $this->input->readBool($item['value']);
-                },
-            TType::BYTE   =>
-                function (&$item, &$xfer) {
-                    $xfer += $this->input->readByte($item['value']);
-                },
-            TType::DOUBLE =>
-                function (&$item, &$xfer) {
-                    $xfer += $this->input->readDouble($item['value']);
-                },
-            TType::I16    =>
-                function (&$item, &$xfer) {
-                    $xfer += $this->input->readI16($item['value']);
-                },
-            TType::I32    =>
-                function (&$item, &$xfer) {
-                    $xfer += $this->input->readI32($item['value']);
-                },
-            TType::I64    =>
-                function (&$item, &$xfer) {
-                    $xfer += $this->input->readI64($item['value']);
-                },
-            TType::STRING =>
-                function (&$item, &$xfer) {
-                    $xfer += $this->input->readString($item['value']);
-                },
-            TType::STRUCT =>
-                function (&$item, &$xfer) {
-                    $class = $item['class'];
-
-                    $item['value'] = new $class();
-
-                    if (isset($item['value']::$_TSPEC)) {
-                        $subArgs = $item['value']::$_TSPEC;
-                        $xfer += $this->read($subArgs);
-
-                        foreach ($subArgs as $subArg) {
-                            if (isset($subArg['value'])) {
-                                $item['value']->{$subArg['var']} = $subArg['value'];
-                            }
-                        }
-                    } else {
-                        throw new \InvalidArgumentException('Invalid argument: ' . $class);
-                    }
-
-                }
-        );
-
-        $callbacks[TType::LST] = function (&$items, &$xfer) use ($callbacks) {
-            $items['value'] = array();
-            $size           = 0;
-            $etype          = 0;
-            $xfer += $this->input->readListBegin($etype, $size);
-
-            for ($i = 0; $i < $size; ++$i) {
-                $item = $items['elem'] + array('value' => null);
-                $callbacks[$items['elem']['type']]($item, $xfer);
-                $items['value'] = $item;
-            }
-            $xfer += $this->input->readListEnd();
-        };
-
-        if (isset($callbacks[$type])) {
-            $callbacks[$type]($item, $xfer);
-        } else {
-            $xfer += $this->input->skip($ftype);
-        }
-    }
-
-    public function sendCommand($command, $args = array())
-    {
-        $this->output->writeMessageBegin($command, TMessageType::CALL, $this->seqid);
-        $this->write($args);
-        $this->output->writeMessageEnd();
-        $this->output->getTransport()->flush();
-    }
-
-    /**
-     * @param array $args
-     * @throws \Thrift\Exception\TApplicationException
-     * @throws \Exception
-     * @return mixed
-     */
-    public function recv($args = array())
-    {
-        $rseqid = 0;
-        $fname  = null;
-        $mtype  = 0;
-
-        $this->input->readMessageBegin($fname, $mtype, $rseqid);
-        if ($mtype == TMessageType::EXCEPTION) {
-            $x = new TApplicationException();
-            $x->read($this->input);
-            $this->input->readMessageEnd();
-            throw $x;
-        }
-
-        $this->read($args);
-        $this->input->readMessageEnd();
-
-        foreach ($args as $arg) {
-            if ($arg['type'] == TType::STRUCT && $arg['value'] instanceof \Exception) {
-                throw $arg['value'];
-            } else if ($arg['value'] !== null) {
-                return $arg['value'];
-            }
-        }
-        return null;
     }
 }
